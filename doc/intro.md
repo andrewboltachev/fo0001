1d623a986019b125f4598227c74a964273e6f319
Andrei Boltachev <andrewboltachev@gmail.com>
2018-11-20 01:37:48 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fo0001
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fo0001/core_test.clj
@@ -0,0 +1,7 @@
+(ns fo0001.core-test
+  (:require [clojure.test :refer :all]
+            [fo0001.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
